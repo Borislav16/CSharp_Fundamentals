6148ecc0bac1aa9d59271b771242961904447655
Borislav16 <borko.g16@gmail.com>
2023-09-23 18:55:43 +0300

    exercise 10

---
+++ DataTypesAndVariables - Lab/10. LowerOrUpper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            char character = char.Parse(Console.ReadLine());
+            result = char.IsLower(character);
+            if(result == false)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
