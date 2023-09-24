34a1304215460fad6df32459f130c23a2d765026
Borislav16 <borko.g16@gmail.com>
2023-09-24 10:19:38 +0300

    exersice 3

---
+++ DataTypesAndVariables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPerson = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)countOfPerson / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
