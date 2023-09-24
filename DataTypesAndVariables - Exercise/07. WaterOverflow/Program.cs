6e74c8dac72b04da4983b62627e1dacec9f46ad3
Borislav16 <borko.g16@gmail.com>
2023-09-24 12:04:09 +0300

    exercise 7

---
+++ DataTypesAndVariables - Exercise/07. WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+            int n = int.Parse(Console.ReadLine());
+            int quantities = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int newQuantitie = int.Parse(Console.ReadLine());
+                if ( newQuantitie + quantities > capacity )
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    
+                }
+                else
+                {
+                    quantities += newQuantitie;
+                }
+            }
+            Console.WriteLine($"{quantities}");
+
+
+        }
+    }
+}
