6691aa38afe62de221037568143d79657dc8be9a
Borislav16 <borko.g16@gmail.com>
2023-09-24 10:24:16 +0300

    exercise 4

---
+++ DataTypesAndVariables - Exercise/04. SumOfChars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < nLines; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                sum += symbol;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
