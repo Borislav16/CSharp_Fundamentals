702f596d7a6e7736b7ad1c0820b91384db9ab53b
Borislav16 <borko.g16@gmail.com>
2023-09-24 10:07:53 +0300

    exersice 2

---
+++ DataTypesAndVariables - Exercise/02. SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while(number > 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
