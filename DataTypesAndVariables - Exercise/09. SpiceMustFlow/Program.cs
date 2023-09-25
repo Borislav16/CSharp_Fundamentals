e9d6a0cffb50a6ad3d12c5519e90c7a7844c319e
Borislav16 <borko.g16@gmail.com>
2023-09-25 13:52:31 +0300

    exercise 9*
    a little more complicated task they are only if you want

---
+++ DataTypesAndVariables - Exercise/09. SpiceMustFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09._SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int yield = 0;
+            int daysCount = 0;
+            while (startingYield >= 100) 
+            {
+                yield += startingYield - 26;
+                startingYield -= 10;
+                daysCount++;
+                
+                
+            }
+            yield -= 26;
+            if(yield < 0)
+            {
+                yield = 0;
+            }
+            Console.WriteLine(daysCount);
+            Console.WriteLine(yield);
+        }
+    }
+}
