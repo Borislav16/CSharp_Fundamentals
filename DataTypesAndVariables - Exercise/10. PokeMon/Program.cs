de1ac9f17cc7e5c166eeb913cfb108e5c1b4f193
Borislav16 <borko.g16@gmail.com>
2023-09-26 13:57:17 +0300

    exercise 10*

---
+++ DataTypesAndVariables - Exercise/10. PokeMon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._PokeMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int originalValue = n;
+            int targetsCount = 0;
+            while(n >= m)
+            {
+                n -= m;
+                targetsCount++;
+                if(n == originalValue / 2)
+                {
+                    if(y != 0)
+                    {
+                        n /= y;
+                    }
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
