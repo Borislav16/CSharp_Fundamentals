c7a5644eb6b41de90398c681798cafe1dd785bc2
Borislav16 <borko.g16@gmail.com>
2023-09-30 09:00:27 +0300

    exercise 8

---
+++ Arrays/08. CondenseArrayToNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._CondenseArrayToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count = array.Length;
+            while (count > 1)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    array[i] = array[i] + array[i + 1];
+                    
+                }
+
+                Console.WriteLine();
+                count--;
+            }
+            Console.WriteLine(array[0]);
+        }
+    }
+}
