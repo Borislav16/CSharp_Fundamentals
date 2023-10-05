542f795aef16736a2c6671a18dd8c8d46e405168
Borislav16 <borko.g16@gmail.com>
2023-10-05 15:09:24 +0300

    exercise 3

---
+++ Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Calculation(command, firstNum, secondNum);
+
+        }
+        static void Calculation(string command, int firstNum, int secondNum)
+        {
+            switch(command)
+            {
+                case "add":
+                    Console.WriteLine(firstNum + secondNum);
+                    break;
+                case "subtract":
+                    Console.WriteLine(firstNum - secondNum);
+                    break;
+                case "multiply":
+                    Console.WriteLine(firstNum * secondNum);
+                    break;
+                case "divide":
+                    Console.WriteLine(firstNum / secondNum);
+                    break;
+            }
+        }
+    }
+}
