69034f3d749f8152bba0e6acd2430f55ed780ee0
Borislav16 <borko.g16@gmail.com>
2023-10-05 15:25:56 +0300

    exercise 5

---
+++ Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            Order(product, quantity);
+        }
+        static void Order(string product, int quantity)
+        {
+            switch(product)
+            {
+                case "coffee":
+                    Console.WriteLine($"{1.50 * quantity:F2}");
+                    break;
+                case "water":
+                    Console.WriteLine($"{1.00 * quantity:F2}");
+                    break;
+                case "coke":
+                    Console.WriteLine($"{1.40 * quantity:F2}");
+                    break;
+                case "snacks":
+                    Console.WriteLine($"{2.00 * quantity:F2}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
