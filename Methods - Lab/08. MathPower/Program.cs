c1d960f42db739134d0611bf196000d948a1ec60
Borislav16 <borko.g16@gmail.com>
2023-10-05 15:41:31 +0300

    exercise 8

---
+++ Methods - Lab/08. MathPower/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            RaiseToPower(number, power);
+        }
+        static void RaiseToPower(double number , double power )
+        {
+            Console.WriteLine(Math.Pow(number, power));
+        }
+    }
+}
