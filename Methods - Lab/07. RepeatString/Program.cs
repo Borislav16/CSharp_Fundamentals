d60839a98e75671d28a7859e36c545fc90f24245
Borislav16 <borko.g16@gmail.com>
2023-10-05 15:33:03 +0300

    exercise 7

---
+++ Methods - Lab/07. RepeatString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07._RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            RepeatString(text, n);
+        }
+        static void RepeatString(string text, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(text);
+            }
+        }
+    }
+}
