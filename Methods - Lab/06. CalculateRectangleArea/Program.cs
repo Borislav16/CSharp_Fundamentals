dd4ca1fa3c602c1b6280d4fa988f2ea54f8c96e6
Borislav16 <borko.g16@gmail.com>
2023-10-05 15:29:43 +0300

    exercise 6

---
+++ Methods - Lab/06. CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int result = RectangleArea(height, width);
+            Console.WriteLine(result);
+        }
+        static int RectangleArea(int height, int width)
+        {
+            return height * width;
+        }
+    }
+}
