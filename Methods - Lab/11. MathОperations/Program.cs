d743f501d75187279e192abf0899f3ac81eae450
Borislav16 <borko.g16@gmail.com>
2023-10-06 18:22:14 +0300

    exercise 11

---
+++ Methods - Lab/11. MathОperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11._MathОperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            MathOperations(firstNum, symbol, secondNum);
+        }
+        static void MathOperations(int firstNum, char symbol, int secondNum)
+        {
+            switch(symbol)
+            {
+                case '+':
+                    Console.WriteLine(firstNum + secondNum);
+                    break;
+                case '*':
+                    Console.WriteLine(firstNum * secondNum);
+                    break;
+                case '-':
+                    Console.WriteLine(firstNum - secondNum);
+                    break;
+                case '/':
+                    Console.WriteLine(firstNum / secondNum);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
