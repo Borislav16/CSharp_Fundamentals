bc6cca6bdc0d17240e2f78ff9ab9dea633e486fe
Borislav16 <borko.g16@gmail.com>
2023-10-08 14:21:16 +0300

    exercise 6

---
+++ Methods - Exercise/06. MiddleCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] array = text.ToCharArray();
+            MiddleChar(array);
+        }
+
+        static void MiddleChar(char[] array)
+        {
+            if (array.Length % 2 == 0)
+            {
+                Console.Write(array[array.Length / 2 - 1]);
+                Console.Write(array[array.Length / 2]);
+            }
+            else
+            {
+                Console.Write(array[(array.Length / 2)]);
+            }
+        }
+    }
+}
