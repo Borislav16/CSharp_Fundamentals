0a10a950abe722e0f80ba1f0f8013ee086ad3d69
Borislav16 <borko.g16@gmail.com>
2023-10-08 14:08:39 +0300

    exercise 5

---
+++ Methods - Exercise/05. AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int add = Add(number1, number2);
+            int subtract = Subtract(add, number3);
+            Console.WriteLine(subtract);
+        }
+
+        static int Subtract(int add, int number3)
+        {
+            return add - number3;
+        }
+
+        static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
