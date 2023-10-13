4d7ecba3dc30e4fec05741e677748f1f7039055e
Borislav16 <borko.g16@gmail.com>
2023-10-13 17:15:00 +0300

    exercise 3

---
+++ Lists - Lab/03. MergingLists/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+
+            List<int> list2 = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+
+            List<int> result = new List<int>();
+
+            int bigger = Math.Max(list1.Count, list2.Count);
+            for (int i = 0; i < bigger; i++)
+            {
+                if(i < list1.Count)
+                {
+                    result.Add(list1[i]);
+                }
+                if (i < list2.Count)
+                {
+                    result.Add(list2[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
