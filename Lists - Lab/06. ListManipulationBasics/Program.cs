0c69a573112fdc92afad9f7daf99c6639372f1aa
Borislav16 <borko.g16@gmail.com>
2023-10-13 17:52:48 +0300

    exercise 6

---
+++ Lists - Lab/06. ListManipulationBasics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace _06._ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string line = default;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                List<string> splited = line.Split(' ').ToList();
+                switch (splited[0])
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(splited[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+
+                    case "Remove":
+                        int numberToRemove = int.Parse(splited[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+
+                    case "RemoveAt":
+                        int indexToRemoveAt = int.Parse(splited[1]);
+                        numbers.RemoveAt(indexToRemoveAt);
+                        break;
+
+                    case "Insert":
+                        int numberToInsert = int.Parse(splited[1]);
+                        int indexToInsert = int.Parse(splited[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
