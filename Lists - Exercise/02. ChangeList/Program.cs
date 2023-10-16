ef2c64b45062d658bf41c58d8dac45240895922a
Borislav16 <borko.g16@gmail.com>
2023-10-16 14:01:41 +0300

    exercise 2

---
+++ Lists - Exercise/02. ChangeList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(' ')
+                 .Select(int.Parse)
+                 .ToList();
+
+            string command = default;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] array = command.Split(' ');
+                switch(array[0])
+                {
+                    case "Delete":
+                        int element = int.Parse(array[1]);
+                        numbers.Remove(element);
+                        break;
+
+                    case "Insert":
+                        int newElement = int.Parse(array[1]);
+                        int position = int.Parse(array[2]);
+                        numbers.Insert(position, newElement);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
