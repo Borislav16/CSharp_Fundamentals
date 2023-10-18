4e8163f9f763ec2edecc5971b019a12140d03cd8
Borislav16 <borko.g16@gmail.com>
2023-10-18 18:54:57 +0300

    exercise 4

--- Lists - Exercise/04. ListOperations/Program.cs
+++ Lists - Exercise/04. ListOperations/Program.cs
@@ -8,42 +8,69 @@ namespace _04._ListOperations
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine().
-                Split(' ')
-                .Select(int.Parse)
-                .ToList();
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
             string command = default;
-            while((command = Console.ReadLine()) != "End")
+            while ((command = Console.ReadLine()) != "End")
             {
-                string[] array = command.Split(' ');
-                switch(array[0])
+                string[] array = command.Split();
+                string action = array[0];
+                if (action == "Add")
                 {
-                    case "Add":
-                        int addToList = int.Parse(array[1]);
-                        numbers.Add(addToList);
-                        break;
-
-                    case "Insert":
-                        int numToList = int.Parse(array[1]);
-                        int indexToList = int.Parse(array[2]);
-                        numbers.Insert(indexToList, numToList);
-                        break;
-
-                    case "Remove":
-                        int removeIndex = int.Parse(array[1]);
-                        numbers.RemoveAt(removeIndex);
-                        break;
-                    case "Shift":
-                        if (array[1] == "left")
-                        {
-
-                        }
-                        else
-                        {
-
-                        }
-                        break;
+                    int task = int.Parse(array[1]);
+                    numbers.Add(task);
                 }
+                else if (action == "Insert")
+                {
+                    int task = int.Parse(array[1]);
+                    int index = int.Parse(array[2]);
+                    if (index >= 0 && index < numbers.Count)
+                    {
+                        numbers.Insert(index, task);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                }
+                else if (action == "Shift")
+                {
+                    string direction = array[1];
+                    if (direction == "left")
+                    {
+                        int task = int.Parse(array[2]);
+                        while (task != 0)
+                        {
+                            int currentnumber = numbers[0];
+                            numbers.RemoveAt(0);
+                            numbers.Add(currentnumber);
+                            task--;
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        int task = int.Parse(array[2]);
+                        while (task != 0)
+                        {
+                            int currentnumber = numbers[numbers.Count - 1];
+                            numbers.RemoveAt(numbers.Count - 1);
+                            numbers.Insert(0, currentnumber);
+                            task--;
+                        }
+                    }
+                }
+                else if (action == "Remove")
+                {
+                    int task = int.Parse(array[1]);
+                    if (task >= 0 && task < numbers.Count)
+                    {
+                        numbers.RemoveAt(task);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                }
+
             }
             Console.WriteLine(string.Join(" ", numbers));
         }
