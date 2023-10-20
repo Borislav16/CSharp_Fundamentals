afeeb2525bf1770eedae4372532c5c8853942bc9
Borislav16 <borko.g16@gmail.com>
2023-10-20 17:08:21 +0300

    exercise 7

---
+++ Lists - Exercise/07. AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> numbers = new List<string>();
+            for (int i = text.Count - 1; i >= 0; i--)
+            {
+                string[] splitedText = text[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                numbers.AddRange(splitedText);
+
+            }
+            
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
