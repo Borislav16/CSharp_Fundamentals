9387a597220302170889ac3ed3dfce7420edd614
Borislav16 <borko.g16@gmail.com>
2023-10-26 09:42:29 +0300

    Objects and Classes

---
+++ Objects and Classes - Lab/01. RandomizeWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = Console.ReadLine().Split().ToList();
+            Random random = new Random();
+            for (int i = 0; i < text.Count - 1; i++)
+            {
+                int randomIndex = random.Next(text.Count);
+
+                string currentWord = text[i];
+                string randomWord = text[randomIndex];
+
+                text[i] = randomWord;
+                text[randomIndex] = currentWord;
+
+            }
+
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
