59966793f24b6adb9ca1c117835000d7bd105dc8
Borislav16 <borko.g16@gmail.com>
2023-11-02 10:51:07 +0200

    Associative Arrays - Lab

---
+++ Associative Arrays - Lab/01. CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int,int> numberOccurences = new SortedDictionary<int,int>();
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            foreach (int number in numbers)
+            {
+                if(!numberOccurences.ContainsKey(number))
+                {
+                    numberOccurences.Add(number, 1);
+                
+                }
+                else
+                {
+                    numberOccurences[number]++;
+                }
+            }
+
+            
+            foreach (KeyValuePair<int,int> kvp in numberOccurences)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            } 
+            
+        }
+    }
+}
