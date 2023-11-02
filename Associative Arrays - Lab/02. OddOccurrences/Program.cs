3dd48322ed1075883a5ae752975e9c1898772da2
Borislav16 <borko.g16@gmail.com>
2023-11-02 11:00:11 +0200

    exercise 2

---
+++ Associative Arrays - Lab/02. OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if(!counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase] = 1;
+                }
+                else
+                {
+                    counts[wordInLowerCase]++;
+                }
+            }
+            foreach (KeyValuePair<string,int> kvp in counts)
+            {
+                if(!(kvp.Value % 2 == 0))
+                {
+                    Console.Write($"{kvp.Key} ");
+                }
+            
+            }
+        }
+    }
+}
