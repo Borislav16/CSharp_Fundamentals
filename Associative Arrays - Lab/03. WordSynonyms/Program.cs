f7fe8cb83bb11b91ccdb466bfd91c2079d92e334
Borislav16 <borko.g16@gmail.com>
2023-11-03 08:51:11 +0200

    exercise 3

---
+++ Associative Arrays - Lab/03. WordSynonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> wordbook = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+                if (!wordbook.ContainsKey(word))
+                {
+                    wordbook.Add(word, synonyms);
+                }
+                else
+                {
+                    wordbook[word] += $", {synonyms}";
+                }
+            }
+
+            foreach (KeyValuePair<string,string> word in wordbook)
+            {
+                Console.WriteLine($"{word.Key} - {word.Value}");
+            }
+        }
+    }
+}
