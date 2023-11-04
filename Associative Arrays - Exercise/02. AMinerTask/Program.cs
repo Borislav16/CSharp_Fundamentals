19ac86a1ec28d986242ef8033a8a36a199e11c09
Borislav16 <borko.g16@gmail.com>
2023-11-04 09:08:23 +0200

    exercise 2

---
+++ Associative Arrays - Exercise/02. AMinerTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, uint> resourses = new Dictionary<string, uint>();
+
+            string input;
+            while((input = Console.ReadLine()) != "stop")
+            {
+                
+                if(!resourses.ContainsKey(input))
+                {
+                    resourses.Add(input, 0);
+                
+                }
+                uint quantity = uint.Parse(Console.ReadLine());
+                resourses[input] += quantity;
+                
+            
+            }
+            foreach (KeyValuePair<string, uint> kvp in resourses)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
