09cce033c5652ae897036937f20b15a7d054ecf9
Borislav16 <borko.g16@gmail.com>
2023-11-08 09:35:42 +0200

    Text Processing - Lab

---
+++ Text Processing - Lab/01. ReverseStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._ReverseStrings
+{
+    class Reverse
+    {
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            string reversed = "";
+            while ((word = Console.ReadLine()) != "end")
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversed += word[i];
+                }
+                Console.WriteLine($"{word} = {reversed}");
+                reversed = "";
+            
+            }
+        }
+    }
+}
