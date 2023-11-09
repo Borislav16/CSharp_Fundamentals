5e3e914fc94b4f8b8e682c5d900f6da055a41bb9
Borislav16 <borko.g16@gmail.com>
2023-11-09 09:50:08 +0200

    exercise 2

---
+++ Text Processing - Lab/02. RepeatStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            string result = "";
+            foreach (string item in array)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    result += item;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
