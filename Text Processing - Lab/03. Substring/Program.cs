2ff89d0776d9137f079ca13cd3cef95b73dd0cd4
Borislav16 <borko.g16@gmail.com>
2023-11-09 09:59:44 +0200

    exercise 3

---
+++ Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            while(input.Contains(filter))
+            {
+                int startIndex = input.IndexOf(filter);
+                input = input.Remove(startIndex,filter.Length);
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
