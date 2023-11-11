6f4440fa3f924d72397c6d3df61d7b78a19ac7fe
Borislav16 <borko.g16@gmail.com>
2023-11-11 13:26:06 +0200

    exercise 2

---
+++ Text Processing - Exercise/02. CharacterMultiplier/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02._CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(" "); 
+            string str1 = array[0];
+            string str2 = array[1];
+            int sum = 0;
+            if(str1.Length >= str2.Length)
+            {
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if (i >= str2.Length)
+                    {
+                        sum += str1[i];
+                    }
+                    else
+                    {
+                        int multiplication = str1[i] * str2[i];
+                        sum += multiplication;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < str2.Length; i++)
+                {
+                    if (i >= str1.Length)
+                    {
+                        sum += str2[i];
+                    }
+                    else
+                    {
+                        int multiplication = str1[i] * str2[i];
+                        sum += multiplication;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
