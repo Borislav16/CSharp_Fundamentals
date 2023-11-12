02255f04f1595dc8d0a49bd738b2b8976e7f5dc9
Borislav16 <borko.g16@gmail.com>
2023-11-12 09:44:39 +0200

    exercise 4

---
+++ Text Processing - Exercise/04. CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04._CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+                sb.Append((char)(symbol + 3));
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
