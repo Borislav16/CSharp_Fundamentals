69c1a5ea71a87b23b4c3971102a7dc64af70008b
Borislav16 <borko.g16@gmail.com>
2023-11-13 08:37:45 +0200

    exercise 5

---
+++ Text Processing - Exercise/05. MultiplyBigNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace _05._MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ulong number = ulong.Parse(Console.ReadLine());
+            ulong secondNumber = ulong.Parse(Console.ReadLine());
+            Console.WriteLine(number * secondNumber);
+        }
+    }
+}
