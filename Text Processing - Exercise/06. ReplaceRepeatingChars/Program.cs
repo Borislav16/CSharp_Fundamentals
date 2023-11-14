d0bdc9fbc795a285e44db7d3cf460d5096e2f921
Borislav16 <borko.g16@gmail.com>
2023-11-14 09:05:00 +0200

    exercise 6

---
+++ Text Processing - Exercise/06. ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _06._ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(input[0]);
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    stringBuilder.Append(input[i]);
+                }
+            }
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
