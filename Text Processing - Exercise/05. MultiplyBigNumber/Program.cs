a3e245fc124f1a0b5dc3e144fd1033cf50b3007a
Borislav16 <borko.g16@gmail.com>
2023-11-14 08:56:35 +0200

    exercise 5

--- Text Processing - Exercise/05. MultiplyBigNumber/Program.cs
+++ Text Processing - Exercise/05. MultiplyBigNumber/Program.cs
@@ -7,10 +7,38 @@ namespace _05._MultiplyBigNumber
     {
         static void Main(string[] args)
         {
-            
-            ulong number = ulong.Parse(Console.ReadLine());
-            ulong secondNumber = ulong.Parse(Console.ReadLine());
-            Console.WriteLine(number * secondNumber);
+            string bigNum = Console.ReadLine();
+            string multiplyNum = Console.ReadLine();
+            Console.WriteLine(MultiplyNumbers(bigNum, multiplyNum));
+        }
+
+        private static string MultiplyNumbers(string bigNum, string multiplyNum)
+        {
+            if (bigNum == "0" || multiplyNum == "0")
+            {
+                return "0";
+
+            }
+
+            int carry = 0;
+            int multiplier = int.Parse(multiplyNum);
+
+            char[] resultChars = new char[bigNum.Length + 1];
+
+            for (int i = bigNum.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(bigNum[i].ToString());
+                int product = digit * multiplier + carry;
+
+                resultChars[i + 1] = (char)(product % 10 + '0');
+                carry = product / 10;
+            }
+            if(carry > 0)
+            {
+                resultChars[0] = (char)(carry + '0');
+            }
+            return new string(resultChars).TrimStart('\0');
+
         }
     }
 }
