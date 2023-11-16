0d29af3f7eab51d922d3a10ae771d595289f66e8
Borislav16 <borko.g16@gmail.com>
2023-11-16 09:27:25 +0200

    Regular Expressions - Lab

---
+++ Regular Expressions - Lab/01. MatchFullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Groups[0]} {match.Groups[1]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
