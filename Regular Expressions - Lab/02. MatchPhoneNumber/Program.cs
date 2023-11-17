345c4ccb4f369597ae677bf0b147cbba06ac00e5
Borislav16 <borko.g16@gmail.com>
2023-11-17 11:43:00 +0200

    exercise 2

---
+++ Regular Expressions - Lab/02. MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            string input = Console.ReadLine();
+            string pattern = @"((\+359 2) (\d{3}) (\d{4})\b)|((\+359-2)-(\d{3})-(\d{4})\b)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                list.Add(match.Value);
+            }
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
