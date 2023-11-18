a36f91a598a9b92cdc9cf32985d436abf65967b6
Borislav16 <borko.g16@gmail.com>
2023-11-18 12:43:36 +0200

    exercise 3

---
+++ Regular Expressions - Lab/03. MatchDates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03._MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(?<day>\d{2})(\/|\.|\-)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+            MatchCollection matches = Regex.Matches(input,pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"].Value}, Month: {match.Groups["month"].Value}, Year: {match.Groups["year"].Value}");
+            }
+        }
+    }
+}
