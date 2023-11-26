3265b2e54ef08cbfb52f3205e9c89ab7a7de72b5
Borislav16 <borko.g16@gmail.com>
2023-11-26 12:12:56 +0200

    exercise 1

--- Exam Preparation 2/1. SecretChat/Program.cs
+++ Exam Preparation 2/1. SecretChat/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace _1._SecretChat
 {
@@ -6,7 +10,42 @@ namespace _1._SecretChat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string command = string.Empty;
+            while((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] splitedCommand = command.Split(":|:");
+                if (splitedCommand[0] == "InsertSpace")
+                {
+                    
+                    int givenIndex = int.Parse(splitedCommand[1]);
+                    input = input.Insert(givenIndex, " ");
+                }
+                else if (splitedCommand[0] == "ChangeAll")
+                {
+                    string substring = splitedCommand[1];
+                    string replacement = splitedCommand[2];
+                    input = input.Replace(substring, replacement);
+                }
+                else if (splitedCommand[0] == "Reverse")
+                {
+                    string substring = splitedCommand[1];
+                    int substringIndex = input.IndexOf(substring);
+                    if(substringIndex < 0)
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+
+                    input = input.Remove(substringIndex, substring.Length);
+                    string reversedSubstring = new string(substring.Reverse().ToArray());
+                    input += reversedSubstring;
+                }
+
+                Console.WriteLine(input);
+            }
+
+            Console.WriteLine($"You have a new text message: {input}");
         }
     }
 }
