9ac2b1ae52772c6243b99924ca52df4f35db77fc
Borislav16 <borko.g16@gmail.com>
2023-12-01 10:33:30 +0200

    exercise 5

---
+++ Exam Preparation 2/05. EmojiDetector/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace _05._EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> emojis = new List<string>();    
+            string coolPattern = @"\d";
+            string emojiPattern = @"(\:{2}|\*{2})(?<emoji>[A-Z][a-z]{2,})\1";
+            ulong coolNumber = 1;
+
+            string input = Console.ReadLine();
+            foreach (Match match in Regex.Matches(input, coolPattern))
+            {
+                coolNumber *= ulong.Parse(match.Value);
+            }
+
+
+            int emojiCount = 0;
+            foreach (Match match in Regex.Matches(input, emojiPattern))
+            {
+                ulong sum = 0;
+                string emoji = match.Groups["emoji"].Value;
+                emojiCount ++;
+                for (int i = 0; i < emoji.Length; i++)
+                {
+                    sum += emoji[i];
+                }
+                if(sum >= coolNumber)
+                {
+                    emojis.Add(match.Value);
+                }
+            }
+            Console.WriteLine($"Cool threshold: {coolNumber}");
+            Console.WriteLine($"{emojiCount} emojis found in the text. The cool ones are:");
+            foreach(string emoji in emojis)
+            {
+                Console.WriteLine(emoji);
+            }
+        }
+    }
+}
